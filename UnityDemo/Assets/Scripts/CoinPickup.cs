d25716bfaf4f30ef5bb779158bb0ef62b69d0b85
Michael Quandt <michael@mquandt.com>
2014-06-09 02:12:26 +1000

    Added coin pickup (no value yet)

---
+++ UnityDemo/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour
+{
+    public float RotationSpeed = 50;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * RotationSpeed);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            // This script is only on the player
+            Destroy(gameObject);
+        }
+    }
+}
