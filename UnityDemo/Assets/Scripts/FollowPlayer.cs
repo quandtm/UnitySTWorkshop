b2f84fd4258d10190c1f9591046be9b758af7625
Michael Quandt <michael@mquandt.com>
2014-06-09 02:44:16 +1000

    Camera rotation

--- UnityDemo/Assets/Scripts/FollowPlayer.cs
+++ UnityDemo/Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,9 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject Player;
 
+    public float RotationSpeed = 50;
+    private float _rotation;
+
     void Awake()
     {
     }
@@ -12,7 +15,19 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Player.transform.position + new Vector3(0, 5, -10);
+        transform.position = Player.transform.position + new Vector3(0, 5, -8);
         transform.LookAt(Player.transform);
+        transform.RotateAround(Player.transform.position, Vector3.up, _rotation);
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            // Right
+            _rotation += Time.deltaTime * RotationSpeed;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            // Left
+            _rotation -= Time.deltaTime * RotationSpeed;
+        }
     }
 }
