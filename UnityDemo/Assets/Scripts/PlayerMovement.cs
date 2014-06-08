444af1658ac154eed539560096337d0b57e719c1
Michael Quandt <michael@mquandt.com>
2014-06-09 01:38:26 +1000

    More of the demo, bouncy ball that moves

---
+++ UnityDemo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Power = 10;
+    public float JumpPower = 300;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            var forward = transform.position - Camera.main.transform.position;
+            forward.Normalize();
+            rigidbody.AddForce(forward * (Power * Time.deltaTime));
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            var backward = Camera.main.transform.position - transform.position;
+            backward.Normalize();
+            rigidbody.AddForce(backward * (Power * Time.deltaTime));
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector3.up * JumpPower);
+        }
+    }
+}
