18308369e3488d0c2b95d1a161e4f762c472256d
Michael Quandt <michael@mquandt.com>
2014-06-13 17:39:30 +1000

    Added app settings

--- Windows81Solution/UnityDemo/App.xaml.cs
+++ Windows81Solution/UnityDemo/App.xaml.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Store;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.ApplicationSettings;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -20,83 +23,93 @@ using UnityPlayer;
 
 namespace Template
 {
-	/// <summary>
-	/// Provides application-specific behavior to supplement the default Application class.
-	/// </summary>
-	sealed partial class App : Application
-	{
-		private WinRTBridge.WinRTBridge _bridge;
-		private AppCallbacks appCallbacks;
-		/// <summary>
-		/// Initializes the singleton application object.  This is the first line of authored code
-		/// executed, and as such is the logical equivalent of main() or WinMain().
-		/// </summary>
-		public App()
-		{
-			this.InitializeComponent();
-			appCallbacks = new AppCallbacks(false);
-			appCallbacks.RenderingStarted += RemoveSplashScreen;
-		}
-
-		/// <summary>
-		/// Invoked when application is launched through protocol.
-		/// Read more - http://msdn.microsoft.com/library/windows/apps/br224742
-		/// </summary>
-		/// <param name="args"></param>
-        protected override void OnActivated(IActivatedEventArgs args)
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        private WinRTBridge.WinRTBridge _bridge;
+        private AppCallbacks appCallbacks;
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
         {
-			string appArgs = "";
-			Windows.ApplicationModel.Activation.SplashScreen splashScreen = null;
-			switch (args.Kind)
-			{
-				case ActivationKind.Protocol:
-					ProtocolActivatedEventArgs eventArgs = args as ProtocolActivatedEventArgs;
-					splashScreen = eventArgs.SplashScreen;
-					appArgs += string.Format("Uri={0}", eventArgs.Uri.AbsoluteUri);
-					break;
-			}
-			InitializeUnity(appArgs, splashScreen);
+            this.InitializeComponent();
+            appCallbacks = new AppCallbacks(false);
+            appCallbacks.RenderingStarted += RemoveSplashScreen;
         }
 
-		/// <summary>
-		/// Invoked when the application is launched normally by the end user.  Other entry points
-		/// will be used when the application is launched to open a specific file, to display
-		/// search results, and so forth.
-		/// </summary>
-		/// <param name="args">Details about the launch request and process.</param>
-		protected override void OnLaunched(LaunchActivatedEventArgs args)
-		{
-			InitializeUnity(args.Arguments, args.SplashScreen);
-		}
+        /// <summary>
+        /// Invoked when application is launched through protocol.
+        /// Read more - http://msdn.microsoft.com/library/windows/apps/br224742
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            string appArgs = "";
+            Windows.ApplicationModel.Activation.SplashScreen splashScreen = null;
+            switch (args.Kind)
+            {
+                case ActivationKind.Protocol:
+                    ProtocolActivatedEventArgs eventArgs = args as ProtocolActivatedEventArgs;
+                    splashScreen = eventArgs.SplashScreen;
+                    appArgs += string.Format("Uri={0}", eventArgs.Uri.AbsoluteUri);
+                    break;
+            }
+            InitializeUnity(appArgs, splashScreen);
+        }
 
-		private void InitializeUnity(string args, Windows.ApplicationModel.Activation.SplashScreen splashScreen)
-		{
-			appCallbacks.SetAppArguments(args);
-			Frame rootFrame = Window.Current.Content as Frame;
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            InitializeUnity(args.Arguments, args.SplashScreen);
+        }
 
-			// Do not repeat app initialization when the Window already has content,
-			// just ensure that the window is active
-			if (rootFrame == null && !appCallbacks.IsInitialized())
-			{
-				var mainPage = new MainPage(splashScreen);
-				Window.Current.Content = mainPage;
-				Window.Current.Activate();
+        private void InitializeUnity(string args, Windows.ApplicationModel.Activation.SplashScreen splashScreen)
+        {
+            appCallbacks.SetAppArguments(args);
+            Frame rootFrame = Window.Current.Content as Frame;
 
-				// Setup scripting bridge
-				_bridge = new WinRTBridge.WinRTBridge();
-				appCallbacks.SetBridge(_bridge);
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null && !appCallbacks.IsInitialized())
+            {
+                var mainPage = new MainPage(splashScreen);
+                Window.Current.Content = mainPage;
+                Window.Current.Activate();
 
-				appCallbacks.SetKeyboardTriggerControl(mainPage);
+                // Setup scripting bridge
+                _bridge = new WinRTBridge.WinRTBridge();
+                appCallbacks.SetBridge(_bridge);
+
+                appCallbacks.SetKeyboardTriggerControl(mainPage);
                 appCallbacks.SetSwapChainPanel(mainPage.GetSwapChainPanel());
 
 
-				appCallbacks.SetCoreWindowEvents(Window.Current.CoreWindow);
+                appCallbacks.SetCoreWindowEvents(Window.Current.CoreWindow);
 
-				appCallbacks.InitializeD3DXAML();
-			}
+                appCallbacks.InitializeD3DXAML();
+            }
 
-			Window.Current.Activate();
-		}
+            SettingsPane.GetForCurrentView().CommandsRequested += OnCommandsRequested;
+
+            Window.Current.Activate();
+        }
+
+        private void OnCommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            args.Request.ApplicationCommands.Add(new SettingsCommand("privacy", "Privacy Policy", command =>
+            {
+                Launcher.LaunchUriAsync(new Uri("http://privacy.policy", UriKind.Absolute));
+            }));
+        }
 
         private void RemoveSplashScreen()
         {
@@ -108,5 +121,5 @@ namespace Template
             catch (InvalidCastException)
             { }
         }
-	}
+    }
 }
